2ae5cdd3cb57c62358319be6034f1bcd71750e73
Harrison Enyeart <enyeart@unc.edu>
2022-08-23 12:11:08 -0400

    Started working on chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,3 +2,9 @@
 
 __author__ = "730566282"
 
+#Part 1. Prompting for Inputs – 20 Points
+word: str = input("Enter a 5-character word: ")
+letter: str = input("Enter a single character: ")
+print("Searching for " + letter + " in " + word)
+
+#Part 2. Checking Indices for Matches – 20 Points
