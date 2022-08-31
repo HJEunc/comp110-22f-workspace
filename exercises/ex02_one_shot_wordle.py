e77bff0fd471d4926604f9e3afea9af27e9f3933
Harrison Enyeart <enyeart@unc.edu>
2022-08-31 15:41:15 -0400

    Finished ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,15 +1,12 @@
-"""EX03 - One Shot Wordle"""
+"""EX03 - One Shot Wordle."""
 
 __author__ = "730566282"
-
-from operator import index
-
 
 secret_word: str = "python"
 guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
 while len(guess) != len(secret_word):
-    guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+    guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -18,20 +15,20 @@ YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
 emoji_result: str = ""
 
-while i < len(secret_word): #tests if indexes match
+while i < len(secret_word):  # tests if indexes match
     if guess[i] == secret_word[i]:
         emoji_result += GREEN_BOX
-    else: #runs if indexes do not math
+    else:  # runs if indexes do not math
         j: int = 0
         checker: bool = False
-        while checker == False and j < len(secret_word): #tests if letters appear at other indexes
+        while not checker and j < len(secret_word):  # tests if letters appear at other indexes
             if guess[i] == secret_word[j]:
                 checker = True
             else:
                 j += 1
-        if checker == True: #runs if letter appears at other index
+        if checker:  # runs if letter appears at other index
             emoji_result += YELLOW_BOX
-        else: #runs if letter does not appear in secret word
+        else:  # runs if letter does not appear in secret word
             emoji_result += WHITE_BOX
     i += 1
 
