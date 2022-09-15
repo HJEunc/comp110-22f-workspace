d35c594b341027921e8ea620828bdc6c9bbfa154
Harrison Enyeart <enyeart@unc.edu>
2022-09-15 10:31:21 -0400

    Finished EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,6 +2,8 @@
 __author__ = "730566282"
 
 def all(input: list[int], integar: int) -> bool:
+    """Given a list of ints and an int, return bool indicating
+    if all int values in list are the same as given int."""
     i: int = 0
     while i < len(input):
         if integar == input[i]:
@@ -11,5 +13,27 @@ def all(input: list[int], integar: int) -> bool:
     return True
 
 def max(input: list[int]) -> int:
+    """Given a list of ints, returns largest int."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
+    max: int = input[0]
+    i: int = 1
+    while i < len(input):
+        if input[i] < max:
+            i += 1
+        else:
+            max = input[i]
+            i += 1
+    return max
+
+def is_equal(input1: list[int], input2: list[int]) -> bool:
+    """Given two lists of ints, returns bool indicating whether they are equal."""
+    if len(input1) != len(input2):
+        return False
+    i: int = 0
+    while i < len(input1):
+        if input1[i] == input2[i]:
+            i += 1
+        else:
+            return False
+    return True
